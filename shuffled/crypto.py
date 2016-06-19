7804339a5dd16a5a069fb9efa80e9975e3aee995
Bertrand Bonnefoy-Claudet <bertrandbc@gmail.com>
2016-06-19 21:08:29 +0200

    Fix domain size and AES block size

--- shuffled/crypto.py
+++ shuffled/crypto.py
@@ -6,8 +6,8 @@ from . import feistel
 
 
 class AesRandomizer:
-    _block_size_bytes = 32
-    domain_size = 2 ** _block_size_bytes
+    _block_size_bytes = 16
+    domain_size = 2 ** (_block_size_bytes * 8)
 
     def __init__(self, key):
         self._cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
